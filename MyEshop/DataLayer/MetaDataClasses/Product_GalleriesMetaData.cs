f8d47afcb6caca24f8b3b7a0793253508d266d93
mohsenDevelop <45555593+mohsenDevelop@users.noreply.github.com>
2019-04-10 07:45:36 -0700

    Create Product_GalleriesMetaData.cs

---
+++ MyEshop/DataLayer/MetaDataClasses/Product_GalleriesMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer
+{
+    public class Product_GalleriesMetaData
+    {
+        [Key]
+        public int GalleryID { get; set; }
+
+        [Display(Name = "کالا")]
+        public int ProductID { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Title { get; set; }
+
+        [Display(Name = "تصویر")]
+        public string ImageName { get; set; }
+    }
+}
