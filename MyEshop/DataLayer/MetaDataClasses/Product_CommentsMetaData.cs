9ccb2e8cb951bbf23444e411bc70e3368edc36d9
mohsenDevelop <45555593+mohsenDevelop@users.noreply.github.com>
2019-04-10 07:45:31 -0700

    Create Product_CommentsMetaData.cs

---
+++ MyEshop/DataLayer/MetaDataClasses/Product_CommentsMetaData.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer
+{
+    public class Product_CommentsMetaData
+    {
+        public int CommentID { get; set; }
+        public int ProductID { get; set; }
+
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Name { get; set; }
+
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress(ErrorMessage ="ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "وب سایت")]
+        public string WebSite { get; set; }
+
+
+        [Display(Name = "متن نظر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Comment { get; set; }
+    }
+}
