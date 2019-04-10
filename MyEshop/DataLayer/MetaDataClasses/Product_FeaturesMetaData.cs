9399dd0ec302774d5eb6184aed5c1f735c456584
mohsenDevelop <45555593+mohsenDevelop@users.noreply.github.com>
2019-04-10 07:45:33 -0700

    Create Product_FeaturesMetaData.cs

---
+++ MyEshop/DataLayer/MetaDataClasses/Product_FeaturesMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer
+{
+    internal class Product_FeaturesMetaData
+    {
+        public int PF_ID { get; set; }
+        [Display(Name = "محصول")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public int ProductID { get; set; }
+
+        [Display(Name = "ویژگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public int FeatureID { get; set; }
+
+        [Display(Name = "مقدار")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Value { get; set; }
+    }
+}
