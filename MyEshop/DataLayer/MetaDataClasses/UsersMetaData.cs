3f902b4ae357d9924f20509e778e95328b0a4f7f
mohsenDevelop <45555593+mohsenDevelop@users.noreply.github.com>
2019-04-10 07:45:53 -0700

    Create UsersMetaData.cs

---
+++ MyEshop/DataLayer/MetaDataClasses/UsersMetaData.cs
@@ -0,0 +1,14 @@
+﻿namespace DataLayer
+{
+    public class UsersMetaData
+    {
+        public int UserID { get; set; }
+        public int RoleID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ActiveCode { get; set; }
+        public bool IsActive { get; set; }
+        public System.DateTime RegisterDate { get; set; }
+    }
+}
