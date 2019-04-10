d8e6dfadc3bc9631559216ade06b987e801ebaa5
mohsenDevelop <45555593+mohsenDevelop@users.noreply.github.com>
2019-04-10 07:45:51 -0700

    Create SliderMetaData.cs

---
+++ MyEshop/DataLayer/MetaDataClasses/SliderMetaData.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer
+{
+    internal class SliderMetaData
+    {
+        [Key]
+        public int SlideID { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Title { get; set; }
+
+
+        [Display(Name = "تصویر")]
+        public string ImageName { get; set; }
+
+        [Display(Name = "تاریخ شروع")]
+        [DisplayFormat(DataFormatString = "{0: yyyy/MM/dd}",ApplyFormatInEditMode =true)]
+        public System.DateTime StartDate { get; set; }
+
+
+        [Display(Name = "تاریخ پایان")]
+        [DisplayFormat(DataFormatString ="{0: yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public System.DateTime EndDate { get; set; }
+
+        [Display(Name = "وضعیت")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [Url(ErrorMessage ="آدرس وارد شده معتبر نمی باشد")]
+        public string Url { get; set; }
+    }
+}
