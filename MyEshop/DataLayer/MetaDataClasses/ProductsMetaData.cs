7fcf289943cbafebc87deae134bf4bd9ca454b0f
mohsenDevelop <45555593+mohsenDevelop@users.noreply.github.com>
2019-04-10 07:45:47 -0700

    Create ProductsMetaData.cs

---
+++ MyEshop/DataLayer/MetaDataClasses/ProductsMetaData.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace DataLayer
+{
+    public class ProductsMetaData
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(350)]
+        public string Title { get; set; }
+
+        [Display(Name = "توضیح مختصر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(500)]
+        [DataType(DataType.MultilineText)]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "متن")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.MultilineText)]
+        [AllowHtml]
+        public string Text { get; set; }
+
+        [Display(Name = "قیمت")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public int Price { get; set; }
+
+        [Display(Name = "تصویر")]
+        public string ImageName { get; set; }
+
+        [Display(Name = "تاریخ ایجاد")]
+        [DisplayFormat(DataFormatString = "{0: yyyy/MM/dd}")]
+        public System.DateTime CreateDate { get; set; }
+    }
+}
