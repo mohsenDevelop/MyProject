9a2e715cf11eb78459c67a2fb421637830ba0742
mohsenDevelop <45555593+mohsenDevelop@users.noreply.github.com>
2019-04-10 07:45:38 -0700

    Create Product_GroupsMetaData.cs

---
+++ MyEshop/DataLayer/MetaDataClasses/Product_GroupsMetaData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer
+{
+    public class Product_GroupsMetaData
+    {
+        [Key]
+        public int GroupID { get; set; }
+
+        [Display(Name = "عنوان گروه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string GroupTitle { get; set; }
+        public Nullable<int> ParentID { get; set; }
+    }
+}
