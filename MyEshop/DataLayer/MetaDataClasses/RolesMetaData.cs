8228b91dc9a34386825e560ded65a19f9c1b43e0
mohsenDevelop <45555593+mohsenDevelop@users.noreply.github.com>
2019-04-10 07:45:49 -0700

    Create RolesMetaData.cs

---
+++ MyEshop/DataLayer/MetaDataClasses/RolesMetaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class RolesMetaData
+    {
+        [Key]
+        public int RoleID { get; set; }
+
+        [Display(Name = "عنوان نقش")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string RoleTitle { get; set; }
+
+        [Display(Name = "عنوان سیستمی نقش")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string RoleName { get; set; }
+    }
+
+    //[MetadataType(typeof(RolesMetaData))]
+    //public partial class Roles
+    //{
+        
+    //}
+}
